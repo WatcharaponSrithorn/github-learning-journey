defc9d80acce48feeb4a6fedc0d2ec0e7dfe6d9e
Watcharapon <srith.watcharapon@gmail.com>
2026-09-12 17:05:14 +0700

    Docs : Add comment in Login.py
    Practice :  using Branch , Pull Request, Merge in this repository
    Add comment explain split process input, condition

--- Login.py
+++ Login.py
@@ -1,7 +1,9 @@
+# innput from user
 username = input("Enter your username : ")
 password = input("Enter your password : ")
 
-if username == "admin" and password == "admin":
+# check condition for login
+if username == "admin" and password == "admin": # check if both 2 conditions are true
     print("Log in sucessful")
 elif username == "admin" and password != "admin":
     print("password incorrect")
