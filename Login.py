9bbe974426450bcaa8857bd1f9d17ff0e358d344
Watcharapon <srith.watcharapon@gmail.com>
2026-09-18 20:30:30 +0700

    Feat : Add code Login.py
    Practice using Issues , Pull Request, Merge

--- Login.py
+++ Login.py
@@ -1,13 +1,15 @@
-# innput from user
+# input from user
 username = input("Enter your username : ")
 password = input("Enter your password : ")
 
 # check condition for login
 if username == "admin" and password == "admin": # check if both 2 conditions are true
-    print("Log in sucessful")
+    print("Log in successful")
 elif username == "admin" and password != "admin":
     print("password incorrect")
 elif username != "admin" and password == "admin":
     print("username incorrect")
 else:
     print("username & password incorrect")
+
+print("End Program")
